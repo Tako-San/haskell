9394c9b6d63b1138da9c352ae8e45006c92024cf
Tako-San <dxe13@mail.ru>
2022-08-13 16:52:58 +0300

    Generators done

---
+++ m3/3_generators/4_Change.hs
@@ -0,0 +1,12 @@
+module Change where
+
+-- coins :: [(Ord a, Num a) => [a]]
+coins :: Num a => [a]
+coins = [2, 3, 7]
+
+change :: (Ord a, Num a) => a -> [[a]]
+change s | s < minimum coins = []
+         | otherwise         = helper s where
+  helper s' | s' < minimum coins = [[]]
+            | otherwise = [ x : xs | x <- coins, xs <- helper (s' - x), sum xs == s' - x ]
+
