a6e4af73234a4418676458a049b63f985146ec44
Tako-San <dxe13@mail.ru>
2022-08-14 13:32:36 +0300

    Provided better solution

--- m3/3_generators/4_Change.hs
+++ m3/3_generators/4_Change.hs
@@ -6,7 +6,17 @@ coins = [2, 3, 7]
 
 change :: (Ord a, Num a) => a -> [[a]]
 change s | s < minimum coins = []
-         | otherwise         = helper s where
-  helper s' | s' < minimum coins = [[]]
-            | otherwise = [ x : xs | x <- coins, xs <- helper (s' - x), sum xs == s' - x ]
+         | otherwise         = helper s   where
+      helper s'
+             | s' < minimum coins
+             = [[]]
+             | otherwise
+             = [ x : xs | x <- coins, xs <- helper (s' - x), sum xs == s' - x ]
+
+change' :: (Ord a, Num a) => a -> [[a]]
+change' s | s == 0            = [[]]
+          | s < minimum coins = []
+          | otherwise         = [ x : xs | x <- coins, xs <- change' (s - x) ]
+
+
 
