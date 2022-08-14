857dceca1b6fe602a4f7caf835d80f442639024a
Tako-San <dxe13@mail.ru>
2022-08-15 01:16:46 +0300

    Started enum types

---
+++ m4/1_enumerationTypes/1_Color.hs
@@ -0,0 +1,7 @@
+module Color where
+
+data Color = Red | Green | Blue
+instance Show Color where
+  show Red = "Red"
+  show Green = "Green"
+  show Blue = "Blue"
---
+++ m4/1_enumerationTypes/2_CharToInt.hs
@@ -0,0 +1,4 @@
+module CharToInt where
+
+charToInt :: Char -> Int
+charToInt c = undefined
